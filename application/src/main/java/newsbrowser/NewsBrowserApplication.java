a0eaf0b074ba19fa373e583f4a2adff324b3f069
adrian216 <adriandurlak9@gmail.com>
2019-04-28 22:41:05 +0200

    separate modules

---
+++ application/src/main/java/newsbrowser/NewsBrowserApplication.java
@@ -0,0 +1,12 @@
+package newsbrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsBrowserApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(NewsBrowserApplication.class, args);
+    }
+}
